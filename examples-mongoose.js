306badd8c77486707e9f5850ab61602550ed1b87
Gabriel Dinse <gabriel.dinse@outlook.com>
2021-08-05 20:30:47 -0300

    Added examples mongoose

--- examples-mongoose.js
+++ examples-mongoose.js
@@ -6,63 +6,77 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true
 })
 
-// const Task = mongoose.model('Task', {
-//     description: {
-//         type: String
-//     },
-//     completed: {
-//         type: Boolean
-//     }
-// })
-
-// const task = new Task({
-//     description: 'task 1',
-//     completed: false
-// })
-
-// task.save().then((result) => {
-//     console.log(task)
-// }).catch((error) => {
-//     console.log(error)
-// })
-
-
-const User = mongoose.model('User', {
-    name: {
+const Task = mongoose.model('Task', {
+    description: {
         type: String,
         required: true,
         trim: true
     },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid.')
-            }
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
+    completed: {
+        type: Boolean,
+        default: false
     }
 })
 
-
-const me = new User({
-    name: '  John  ',
-    email: '   jOhn@Email.CoM   '
+const task = new Task({
+    description: '  some task    '
 })
 
-me.save().then((result) => {
-    console.log(me)
+task.save().then((result) => {
+    console.log(task)
 }).catch((error) => {
-    console.log('Error!', error)
+    console.log(error)
 })
+
+
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         trim: true,
+//         lowercase: true,
+//         validate(value) {
+//             if (!validator.isEmail(value)) {
+//                 throw new Error('Email is invalid.')
+//             }
+//         }
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//         minlength: 7,
+//         trim: true,
+//         validator(value) {
+//             if (value.toLowerCase().includes('password')) {
+//                 throw new Error('Password cannot contain "password"')
+//             }
+//         }
+//     },
+//     age: {
+//         type: Number,
+//         default: 0,
+//         validate(value) {
+//             if (value < 0) {
+//                 throw new Error('Age must be a positive number')
+//             }
+//         }
+//     }
+// })
+
+
+// const me = new User({
+//     name: '  John  ',
+//     email: '   jOhn@Email.CoM   ',
+//     password: "testpasswd"
+// })
+
+// me.save().then((result) => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
