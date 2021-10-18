55c2092e3b10889c10a3ba549a594870970ebe31
Gabriel Dinse <gabriel.dinse@outlook.com>
2021-10-17 22:35:32 -0300

    Added endpoints for getting all tasks and single task by id

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,5 +3,6 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateItndex: true,
+    useUnifiedTopology: true // Not using this will use deprecated engine
 })
--- src/index.js
+++ src/index.js
@@ -18,15 +18,52 @@ app.post('/users', (req, res) => {
     })
 })
 
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        // The only problem which can happen here relates to server side
+        res.status(500).send()
+    })
+})
+
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    console.log(_id)
+    User.findById(_id).then((user) => {
+        res.send(user)
+    }).catch((e) => {
+        return res.status(404).send()
+    })
+})
+
 app.post('/tasks', (req, res) => {
     const task = new Task(req.body)
 
     task.save().then(() => {
         res.status(201).send(task)
-    }).catch(() => {
+    }).catch((e) => {
         res.status(400).send(e)
     })
 })
+
+app.get('/tasks', (req, res) => {
+    Task.find({}).then((tasks) => {
+        res.send(tasks)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.get('/tasks/:id', (req, res) => {
+    const _id = req.params.id
+    Task.findById(_id).then((task) => {
+        res.send(task)
+    }).catch((e) => {
+        res.status(404).send()
+    })
+})
+
 
 app.listen(port, () => {
     console.log('Server is running on port ' + port)
